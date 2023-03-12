d04f58a32a6ae4c58f8574d9cac15d318d74a780
Tristan van Triest <tristantriest@gmail.com>
2023-03-12 16:01:27 +0100

    wip(UI): Added estimated time, description and method inputs to fragment_create.xml

--- app/src/main/java/com/bitebybyte/ui/create/CreateFragment.java
+++ app/src/main/java/com/bitebybyte/ui/create/CreateFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bitebybyte.R;
 import com.bitebybyte.databinding.FragmentCreateBinding;
 
 public class CreateFragment extends Fragment {
@@ -24,8 +27,12 @@ public class CreateFragment extends Fragment {
         binding = FragmentCreateBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textDashboard;
-//        createViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Spinner spinner = binding.spinner;
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.time_steps_array, android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
         return root;
     }
 
