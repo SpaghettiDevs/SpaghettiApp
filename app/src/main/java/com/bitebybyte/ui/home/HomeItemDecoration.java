88893f83e5c0d2b14f0f641121de503efc873753
Alies_20212404 <afrancke03@gmail.com>
2023-03-13 22:43:41 +0100

    feat(homefeed): Implement HomeItemDecoration for custom margin in grid

---
+++ app/src/main/java/com/bitebybyte/ui/home/HomeItemDecoration.java
@@ -0,0 +1,38 @@
+package com.bitebybyte.ui.home;
+
+import android.graphics.Rect;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeItemDecoration extends RecyclerView.ItemDecoration
+{
+    private final int columns;
+    private final int margin;
+
+    HomeItemDecoration(int margin, int columns) {
+        this.margin = margin;
+        this.columns = columns;
+    }
+
+    @Override
+    public void getItemOffsets(@NonNull Rect outRect, @NonNull View view,
+                               @NonNull RecyclerView parent, @NonNull RecyclerView.State state)
+    {
+        int position = parent.getChildLayoutPosition(view);
+        //set right margin to all
+        outRect.right = margin;
+        //set bottom margin to all
+        outRect.bottom = margin;
+        //we only add top margin to the first row
+        if (position <columns) {
+            outRect.top = margin;
+        }
+        //add left margin only to the first column
+        if(position%columns==0){
+            outRect.left = margin;
+        }
+    }
+
+}
