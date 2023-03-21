4d94eafbe850401e2a55779b7a1cfbd495f2ab71
Tristan van Triest <tristantriest@gmail.com>
2023-03-21 10:45:35 +0100

    feat(Logic): Added form handling to create post screen in CreateFragment

--- app/src/main/java/com/bitebybyte/ui/create/CreateFragment.java
+++ app/src/main/java/com/bitebybyte/ui/create/CreateFragment.java
@@ -5,33 +5,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.bitebybyte.R;
 import com.bitebybyte.databinding.FragmentCreateBinding;
 
 public class CreateFragment extends Fragment {
 
+
     private FragmentCreateBinding binding;
+
+    private EditText title;
+    private EditText estimatedTime;
+    private EditText description;
+    private EditText ingredients;
+    private EditText method;
+    private Button submitButton;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        CreateViewModel createViewModel =
-                new ViewModelProvider(this).get(CreateViewModel.class);
-
         binding = FragmentCreateBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Find the spinner in the UI
         Spinner spinner = binding.spinner;
+
+        //Create an adapter to load the spinner items
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
                 R.array.time_steps_array, android.R.layout.simple_spinner_dropdown_item);
 
+        //Apply it to the spinner.
         spinner.setAdapter(adapter);
+
+        //Find the text-inputs
+        title = root.findViewById(R.id.create_post_title_input);
+        estimatedTime = root.findViewById(R.id.create_post_estimated_time_edit_text);
+        description = root.findViewById(R.id.create_post_description);
+        ingredients = root.findViewById(R.id.create_post_ingredients_input);
+        method = root.findViewById(R.id.create_post_method);
+        submitButton = root.findViewById(R.id.create_post_submit_button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Form was submitted
+                //Do validation and send to database
+            }
+        });
 
         return root;
     }
