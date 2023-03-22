6674f631abb9a5df3dc82c7973fba2cbd54015f1
siperinio <104511201+siperinio@users.noreply.github.com>
2023-03-22 15:52:26 +0100

    style(fixed style issues): Fixed style issues according to the pull review

--- app/src/main/java/com/bitebybyte/MainActivity.java
+++ app/src/main/java/com/bitebybyte/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        auth.signOut();
         FirebaseUser currentUser = auth.getCurrentUser();
         if(currentUser == null){
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
