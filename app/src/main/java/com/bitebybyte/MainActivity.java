a56fad28b0662b2a936e6e14a1aad75d9a72cfe3
Alies_20212404 <afrancke03@gmail.com>
2023-03-25 10:07:49 +0100

    fix(sidebar): Rename navView to bottomNavView

--- app/src/main/java/com/bitebybyte/MainActivity.java
+++ app/src/main/java/com/bitebybyte/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController);
 
     }
 
