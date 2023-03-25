91294787ca30cb02b6a16711f2fa6cde74effefd
Alies_20212404 <afrancke03@gmail.com>
2023-03-25 10:06:09 +0100

    refactor(bottomnav): Rename nav_view to bottom_nav_view
    This is to make a distinction between bottom navigation and sidebar

--- app/src/main/java/com/bitebybyte/MainActivity.java
+++ app/src/main/java/com/bitebybyte/MainActivity.java
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        bottomNavigationView = findViewById(R.id.nav_view);
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = new AppBarConfiguration.Builder(
