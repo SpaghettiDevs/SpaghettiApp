f27eb02e5670130a11b7265268f1fc3b736115e1
Thomas Nagel <tmj.nagel@gmail.com>
2023-03-29 01:04:30 +0200

    lemauw feat:"Settings button" settings button in sidebar now links to the settings activity

--- app/src/main/java/com/bitebybyte/SettingsActivity.java
+++ app/src/main/java/com/bitebybyte/SettingsActivity.java
@@ -1,13 +1,27 @@
 package com.bitebybyte;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+
+    private EditText emailText;
+    private EditText usernameText;
+    private EditText passwordText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        auth = FirebaseAuth.getInstance();
+
+        emailText = findViewById(R.id.change_email);
+        usernameText = findViewById(R.id.change_username);
+        passwordText = findViewById(R.id.change_password);
     }
 }
