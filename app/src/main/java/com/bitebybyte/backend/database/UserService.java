ffe87828172f629f24991547eba188aa3f47f4bf
siperinio <104511201+siperinio@users.noreply.github.com>
2023-03-28 13:59:00 +0200

    feat(implemented User and UserService): User fully implemented and test Implementation for UserService

--- app/src/main/java/com/bitebybyte/backend/database/UserService.java
+++ app/src/main/java/com/bitebybyte/backend/database/UserService.java
@@ -1,4 +1,56 @@
 package com.bitebybyte.backend.database;
 
-public class UserService {
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import com.bitebybyte.backend.local.User;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class UserService implements OnSuccessListener, OnFailureListener {
+    FirebaseFirestore db;
+    public UserService() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+    //save a new user to the database
+    public void saveUserToDb(User user) {
+        db.collection("users")
+                .document(user.getUserId()).set(user)
+                .addOnSuccessListener(this)
+                .addOnFailureListener(this);
+    }
+
+    //check if an existant user already has the same username.
+    public boolean usernameCheckQuery(String username) {
+        //query that looks through the users collection and checks the username field
+        Task<QuerySnapshot> task = db.collection("users")
+                .whereEqualTo("username", username)
+                .get();
+
+        //wait for the query to finish. since its async.
+        while (task.isComplete() == false) {}
+
+        //if the result is empty return true that there is no the same username.
+        //if not empty there is someone with the same username.
+        if (task.getResult().isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void onSuccess(Object o) {
+        System.out.println("Successfully added");
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+        System.out.println("Failed add" + e);
+    }
 }
--- app/src/main/java/com/bitebybyte/backend/local/User.java
+++ app/src/main/java/com/bitebybyte/backend/local/User.java
@@ -1,4 +1,24 @@
 package com.bitebybyte.backend.local;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private String UserId;
+    private String username;
+    private List<String> myPosts;
+    private List<String> savedPosts;
+
+    public User(String UserId, String username) {
+        this.UserId = UserId;
+        this.username = username;
+        this.myPosts = new ArrayList<>();
+        this.savedPosts = new ArrayList<>();
+    }
+
+    public String getUserId() { return this.UserId; }
+    public String getUsername() { return this.username; }
+    public List<String> getMyPosts() { return this.myPosts; }
+    public List<String> getSavedPosts() { return this.savedPosts; }
 }
