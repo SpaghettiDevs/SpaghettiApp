619edd9a536e44cc219e4bf64e9c5bfa3ece900f
Tristan van Triest <tristantriest@gmail.com>
2023-03-31 12:59:25 +0200

    feat(logic/UI): Removed labels from create post. Added field checks for empty fields.

--- app/src/main/java/com/bitebybyte/ui/create/CreateFragment.java
+++ app/src/main/java/com/bitebybyte/ui/create/CreateFragment.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -77,6 +78,41 @@ public class CreateFragment extends Fragment {
                 System.out.println("method " + method.getText());
                 System.out.println("Button Pressed! ");
 
+                if (title.getText().toString().equals("Title") || title.getText().toString().isEmpty()) {
+                    Toast.makeText(getContext(), "Filling in a title is required", Toast.LENGTH_SHORT).show();
+                    title.setError("Filling in a title is required");
+                    return;
+                }
+
+                if (description.getText().toString().isEmpty()) {
+                    Toast.makeText(getContext(), "Filling in a description is required", Toast.LENGTH_SHORT).show();
+                    description.setError("Filling in a description is required");
+                    return;
+                }
+
+                if (ingredients.getText().toString().isEmpty()) {
+                    Toast.makeText(getContext(), "Filling in ingredients is required", Toast.LENGTH_SHORT).show();
+                    ingredients.setError("Filling in ingredients is required");
+                    return;
+                }
+
+                if (method.getText().toString().isEmpty()) {
+                    Toast.makeText(getContext(), "Filling in a method is required", Toast.LENGTH_SHORT).show();
+                    method.setError("Filling in a method is required");
+                    return;
+                }
+
+                if (imageURI == null) {
+                    Toast.makeText(getContext(), "Please add an image", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(estimatedTime.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Filling in an estimated time is required", Toast.LENGTH_SHORT).show();
+                    estimatedTime.setError("Filling in an estimated time is required");
+                    return;
+                }
+
                 //creating a post
                 String postID = service.createPostWithRecipe(userService.getUsername(), description.getText().toString(), title.getText().toString(),
                         null, null,
@@ -87,6 +123,9 @@ public class CreateFragment extends Fragment {
                 userService.updateMyPosts(postID);
 
                 service.saveImageToDatabase(imageURI, imageButton, postID);
+
+
+
             }
         });
 
