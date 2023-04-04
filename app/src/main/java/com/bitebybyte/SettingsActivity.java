f427c94c9b6da05bf26c9558d38420cb0c965e79
Thomas Nagel <tmj.nagel@gmail.com>
2023-04-04 11:23:14 +0200

    feat:"settings page" delete change email or password done
    change username still needs to be added
    email verification also needs to be added

--- app/src/main/java/com/bitebybyte/SettingsActivity.java
+++ app/src/main/java/com/bitebybyte/SettingsActivity.java
@@ -1,13 +1,24 @@
 package com.bitebybyte;
 
-import android.app.Dialog;
+
+import static android.app.PendingIntent.getActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseUser;
 import com.jakewharton.processphoenix.ProcessPhoenix;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -15,10 +26,14 @@ public class SettingsActivity extends AppCompatActivity {
     private FirebaseAuth auth;
 
     private EditText emailText;
-    private EditText usernameText;
-    private EditText passwordText;
+    private Button emailButton;
+    private Button passwordButton;
+    private Button deleteAccountButton;
 
-    private Button confirmButton;
+    private FirebaseUser user;
+
+    private String currentEmail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,42 +41,76 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
 
         auth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
-        emailText = findViewById(R.id.change_email);
-        usernameText = findViewById(R.id.change_username);
-        passwordText = findViewById(R.id.change_password);
-        confirmButton = findViewById(R.id.confirm_changes_button);
+        if (user != null) {
+            ((TextView)findViewById(R.id.textViewEmail)).setText(user.getEmail());
+            ((TextView)findViewById(R.id.textViewUsername)).setText(user.getDisplayName());
+            currentEmail = user.getEmail();
+        } else {
+            Toast.makeText(getApplicationContext(), "Error: current user can not be loaded", Toast.LENGTH_SHORT).show();
+            finish();
+        }
 
-        confirmButton.setOnClickListener(new View.OnClickListener() {
+
+        emailText = findViewById(R.id.change_email_text);
+        emailButton = findViewById(R.id.change_email_button);
+        passwordButton = findViewById(R.id.reset_password);
+        deleteAccountButton = findViewById(R.id.delete_account);
+
+        emailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = emailText.getText().toString().trim();
-                String username = usernameText.getText().toString().trim();
-                String password = passwordText.getText().toString().trim();
-
-                // custom dialog
-                final Dialog dialog = new Dialog(getApplicationContext());
-                dialog.setContentView(R.layout.dialog_authenticate);
-                dialog.setTitle("Authenticate to confirm changes");
-
-                /*EditText emailAuth = (EditText) dialog.findViewById(R.id.email);
-                EditText passwordAuth = (EditText) dialog.findViewById(R.id.password);*/
-                dialog.show();
 
                 if(!email.isEmpty()) {
-
+                    user.updateEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()) {
+                                currentEmail = user.getEmail();
+                                auth.signOut();
+                                startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+                                finish();
+                            }
+                        }
+                    });
+                } else {
+                    emailText.setError("Email cannot be empty");
+                    return;
                 }
+            }
+        });
 
-                if(!username.isEmpty()) {
+        passwordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.sendPasswordResetEmail(currentEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()) {
+                            auth.signOut();
+                            startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                });
+            }
+        });
 
-                }
-
-                if(!password.isEmpty()) {
-
-                }
-
-                //Restart application
-                ProcessPhoenix.triggerRebirth(getApplicationContext());
+        deleteAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()) {
+                            auth.signOut();
+                            startActivity(new Intent(SettingsActivity.this, MainActivity.class));
+                            finish();
+                        }
+                    }
+                });
             }
         });
     }
