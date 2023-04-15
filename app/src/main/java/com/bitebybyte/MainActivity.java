1cfc487df58b7c1691d0302818e9c79e32262b25
Tristan <tristantriest@gmail.com>
2023-04-15 12:08:59 +0200

    feat(refactor): Refactored MainActivity

--- app/src/main/java/com/bitebybyte/MainActivity.java
+++ app/src/main/java/com/bitebybyte/MainActivity.java
@@ -26,70 +26,110 @@ import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+/**
+ * Represents the main activity of the app.
+ * All fragments are hosted in this activity.
+ * This activity is the entry point of the app.
+ */
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private FirebaseAuth        auth = FirebaseAuth.getInstance();
-    private UserService userService = new UserService();
+    private FirebaseAuth        auth;
+    private UserService userService;
     private Toolbar             toolbar;
-    private AppBarConfiguration appBarConfiguration;
     private NavController        navController;
     private BottomNavigationView bottomNavigationView;
 
     private DrawerLayout drawerLayout;
 
     private NavigationView sideBar;
-    private TextView sideBarEmail;
-    private TextView sideBarUsername;
 
-    private ImageButton settingsButton;
-
+    /**
+     * Initializes the activity and sets up the UI components.
+     * @param savedInstanceState Saved instance state
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        auth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = auth.getCurrentUser();
 
         //If the user is not logged in, redirect them to the login page
-        if(currentUser == null) {
+        if (currentUser == null) {
             startActivity(new Intent(MainActivity.this, LoginActivity.class));
             finish();
             return;
         }
 
+        userService = new UserService();
 
+        setupUI(currentUser);
+        setupNavController();
+        disableBottomBar(navController);
+        removeHamburgerIcon(navController);
+        setupSettingsButton();
+    }
+
+    /**
+     * Sets up the UI components.
+     */
+    private void setupUI(FirebaseUser currentUser) {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         bottomNavigationView = findViewById(R.id.bottom_nav_view);
         sideBar = findViewById(R.id.side_bar);
         drawerLayout = findViewById(R.id.drawer_layout);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-
-        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
 
         //setting the email on the sidebar
-        sideBarEmail = sideBar.getHeaderView(0).findViewById(R.id.user_email_sidebar);
+        TextView sideBarEmail = sideBar.getHeaderView(0).findViewById(R.id.user_email_sidebar);
         sideBarEmail.setText(currentUser.getEmail());
 
         //setting the username on the sidebar
-        sideBarUsername = sideBar.getHeaderView(0).findViewById(R.id.user_name_sidebar);
+        TextView sideBarUsername = sideBar.getHeaderView(0).findViewById(R.id.user_name_sidebar);
         sideBarUsername.setText(userService.getCurrentUsername());
+    }
 
-        appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_create, R.id.navigation_saved).setOpenableLayout(drawerLayout).build();
+    /**
+     * Sets up the navigation controller.
+     */
+    private void setupNavController() {
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_create, R.id.navigation_saved)
+                .setOpenableLayout(drawerLayout)
+                .build();
 
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(sideBar, navController);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
 
-        // Disable the bottom navigation bar according to the argument "showBottomBar"
-        navController.addOnDestinationChangedListener((navController, destination, arguments) -> {
+    /**
+     * Sets up the settings button.
+     */
+    private void setupSettingsButton() {
+        ImageButton settingsButton = sideBar.getHeaderView(0).findViewById(R.id.account_settings_button);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
+    }
+
+    /**
+     * Disables the bottom navigation bar according to the argument "showBottomBar".
+     * @param navController Navigation controller for the activity
+     */
+    private void disableBottomBar(NavController navController) {
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
             boolean showBottomBar = true; // show bottomBar by default
 
             // Update showBottomBar if it is specified in the navigation graph
@@ -99,30 +139,27 @@ public class MainActivity extends AppCompatActivity {
             }
             bottomNavigationView.setVisibility(showBottomBar ? View.VISIBLE : View.GONE);
         });
+    }
 
-        // Remove hamburger icon when on a top level fragment
+    /**
+     * Removes the hamburger icon when on a top level fragment.
+     * @param navController Navigation controller for the activity
+     */
+    private void removeHamburgerIcon(NavController navController) {
         navController.addOnDestinationChangedListener(
-                (navController, destination, arguments) -> {
+                (controller, destination, arguments) -> {
                     int id = destination.getId();
 
                     if (id == R.id.navigation_home ||
-                        id == R.id.navigation_create ||
-                        id == R.id.navigation_saved)
+                            id == R.id.navigation_create ||
+                            id == R.id.navigation_saved)
                     {
                         toolbar.setNavigationIcon(null);
                     }
 
                 });
-        settingsButton =  sideBar.getHeaderView(0).findViewById(R.id.account_settings_button);
-
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-            }
-        });
-
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
