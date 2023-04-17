a29dec98c20de4e87fdd225cbce9e8b834209d09
siperinio <104511201+siperinio@users.noreply.github.com>
2023-04-17 12:19:26 +0200

    feat(test AbstractContent): Implemented unit tests for AbstractContent

---
+++ app/src/test/java/com/bitebybyte/backend/models/AbstractContentTest.java
@@ -0,0 +1,58 @@
+package com.bitebybyte.backend.models;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AbstractContentTest {
+    private final String ownerId = "tester";
+    private final String content = "testing content";
+    private final AbstractContent instance = new TestContent(ownerId, content);
+
+    public class TestContent extends AbstractContent {
+        public TestContent(String ownerId, String content) {
+            super(ownerId, content);
+        }
+    }
+
+    /* Test for checking that the content is not null when instantiated. */
+    @Test
+    public void getContentNullTest() {
+        assertNotNull(instance.getContent());
+    }
+
+    /* Test for checking that the owner id is not null when instantiated. */
+    @Test
+    public void getOwnerIdNullTest() {
+        assertNotNull(instance.getIdOwner());
+    }
+
+    /* Test for checking that the Date is not null when instantiated. */
+    @Test
+    public void getDateNullTest() {
+        assertNotNull(instance.getDate());
+    }
+
+    /* Test for checking that the likes counter is not null when instantiated. */
+    @Test
+    public void getLikesNullTest() {
+        assertNotNull(instance.getLikes());
+    }
+
+    /* Test for checking that the Post id is not null when instantiated. */
+    @Test
+    public void getPostIdNullTest() {
+        assertNotNull(instance.getPostId());
+    }
+
+    /* Test for checking if the getContent returns the same content as instantiated with. */
+    @Test
+    public void getContentTest() {
+        assertEquals(instance.getContent(), content);
+    }
+
+    /* Test for checking if the getOwnerId returns the same Id as instantiated with. */
+    @Test
+    public void getOwnerIdTest() {
+        assertEquals(instance.getIdOwner(), ownerId);
+    }
+}
